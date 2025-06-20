d5bc61bd0b6a1bb5b652f006569de238943bb9d7
Heaven <154735143+codinghub306@users.noreply.github.com>
2025-06-20 16:10:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,11 +42,13 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.") 
 ADMINS.append(OWNER)
 
-bot = Client("bot",    
-   bot_token="",    
-   api_id= ,    
-   api_hash= ""
+bot = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
+
 
 @bot.on_message(filters.command(["started"]))    
 async def account_login(bot: Client, m: Message):    
